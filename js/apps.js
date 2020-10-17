687414eb5d6d821140e1c2fe613e9250d7200fff
CarissaAyo <ajaoyussufayo@gmail.com>
2020-10-17 07:12:12 +0100

    done with navbar logic

--- js/apps.js
+++ js/apps.js
@@ -1,4 +1,52 @@
-const openNav= document.getElementById('open-nav')
-openNav.addEventListener('click',()=>{
-    console.log('ok')
+let slide = {
+    img1:"images/desktop-image-hero-1.jpg",
+    img2: "images/desktop-image-hero-2.jpg",
+    img3: "images/desktop-image-hero-3.jpg",
+}
+
+const openNav= document.getElementById('open-nav');
+let nav = document.querySelector('.hide-nav');
+let closeNav =document.getElementById('close-nav');
+// the harmburger and the logo con
+let hideHarmburger = document.getElementById('hide-harmburger-logo');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Navbar Events 
+openNav.addEventListener('click',(e)=>{
+    openNavBar()
 })
+
+closeNav.addEventListener('click',()=>{
+   closeNavBar()
+})
+
+// NavBar funtions
+function openNavBar(){
+    nav.classList.toggle('hide-nav');
+    hideHarmburger.classList.toggle('hide-logo');
+}
+function closeNavBar(){
+    nav.classList.toggle('hide-nav');
+    hideHarmburger.classList.toggle('hide-logo');
+}
